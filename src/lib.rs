fa7c97cb26efa46fcd9c166c0671ee70bb1fd137
td-bn <bnabi@bnabis-MacBook-Pro.local>
2022-07-24 20:40:36 +0530

    3.7: Add tests for subscribe, add extractor for HTML form

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    #[allow(dead_code)]
+    email: String,
+    #[allow(dead_code)]
+    name: String,
+}
+
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,53 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_data() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+    let body = "name=bn&email=tdnb%40hello.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute subscription request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20me", "missing the email"),
+        ("email=le%40me.com", "missing the name"),
+        ("", "missing the name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute subscription request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 bad Request with payload {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     // Retreive the port
